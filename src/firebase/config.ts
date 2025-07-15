66e6110511b2fde2cc685dba1c9c9c237968e21a
jsfather <keyvanmatinrad@outlook.com>
2025-07-15 12:12:12 +0330

    credentials moved to env file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcWOkML459EM5nA-MwK1LFSqWDlcmlFIA",
-  authDomain: "leadochat-task.firebaseapp.com",
-  projectId: "leadochat-task",
-  storageBucket: "leadochat-task.firebasestorage.app",
-  messagingSenderId: "99663693481",
-  appId: "1:99663693481:web:496c2ae6d72c65c1c80bb8",
-  measurementId: "G-DDSVXCSH6N",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
